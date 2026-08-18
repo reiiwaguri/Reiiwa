9a8704a6dff94c0a63879f59940c12449cf10dd3
reiiwaguri <iryuukazaya1998@gmail.com>
2026-08-18 15:39:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Isi dua nilai ini dari Supabase.
 // Gunakan ANON/PUBLIC KEY, BUKAN service_role key.
-window.SUPABASE_URL = "https://YOUR-PROJECT.supabase.co";
-window.SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+window.SUPABASE_URL = "https://bokwpryzammxkcjrnqjl.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_yPvDYPBh5VDDQezkHuF59w_NQlWDE6l";
